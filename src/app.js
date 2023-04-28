01af38b48660ad5164a58ad07e221e81b9387a63
MaikelGevdLeeuw <maikelgevdleeuw@outlook.com>
2023-04-28 07:34:11 +0200

    Uitwerkingen

--- src/app.js
+++ src/app.js
@@ -34,9 +34,26 @@ async function fetchData(input){
     try{
         const response = await axios.get(`https://restcountries.com/v3.1/name/${ input }`);
         response.data.map((country => {
-            const lang = Object.values(country.languages)[0];
+
+            //Checking if the lengt of languages array is larger than 1.
+            let lang = Object.values(country.languages);
+            let text = ""
+            if (lang.length < 2){
+                lang = Object.values(country.languages)[0];
+                text = "language"
+            }
+            else if(lang.length === 2){
+                lang = `${Object.values(country.languages)[0]} and ${Object.values(country.languages)[1]}`
+                text = "languages"
+            }
+            else{
+                lang = `${Object.values(country.languages)[0]}, ${Object.values(country.languages)[1]} and ${Object.values(country.languages)[2]}`
+                text = "languages"
+            }
+
+
+
             const curr = Object.values(country.currencies)[0].name;
-            console.log(curr)
             countyInformation.innerHTML =`
                   <div class="outputDiv">
                         <p>Traveling is a journey, <span style="font-weight: bold; color: orange">${country.name.common}</span> to explore,
@@ -45,7 +62,7 @@ async function fetchData(input){
                             And venture beyond, to step out of the known,
                             To leave behind what's familiar, and go on our own.
                             The people we meet, <span style="font-weight: bold; color: orange">${country.population}</span> stories to share,
-                            <span style="font-weight: bold; color: orange">${lang}</span> the language they speak.
+                            <span style="font-weight: bold; color: orange">${lang}</span> the ${text} they speak.
                             To travel is to learn, to see the world anew,
                             Welcome to <span style="font-weight: bold; color: orange">${country.subregion}</span>, the place for you.
                             To broaden our minds, to see what's truly true.
@@ -58,6 +75,25 @@ async function fetchData(input){
         }));
     }
     catch (e) {
+        countyInformation.innerHTML =`
+                  <div class="outputDiv">
+                        <p>Oh no, it seems there's been a mistake,
+                            Perhaps a typo, or a slip-up you did make,
+                            We cannot find the country you seek,
+                            Please try again, and take another peek.
+                            
+                            Double check the spelling, it's important you see,
+                            Or you might be lost, like a ship at sea,
+                            Take your time, and try once more,
+                            And we'll help you find what you're looking for.
+                            
+                            Don't fret, don't fear, we're here to assist,
+                            With patience and care, until the issue is fixed,
+                            So take a deep breath, and give it a go,
+                            And together we'll find the answer, don't you know?
+                        </p>
+                   </div>
+                `;
         console.error(e);
         return null;
     }

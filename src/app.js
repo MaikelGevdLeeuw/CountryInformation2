c350f735ecad5b721f2d72516adbee3e4afc3a62
MaikelGevdLeeuw <maikelgevdleeuw@outlook.com>
2023-04-28 07:46:49 +0200

    Uitwerkingen

--- src/app.js
+++ src/app.js
@@ -15,21 +15,23 @@ button.addEventListener("click", function(event) {
     //Preventing the browser from refreshing
     event.preventDefault();
     void fetchData(input.value);
+    //Clear the input from HTML.
     input.value = "";
 });
-//Using the enterkey to search
+//Using the enterkey to search and call fetchDataFunction
 input.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         //Preventing the browser from refreshing
         event.preventDefault();
         void fetchData(input.value);
+        //Clear the input from HTML.
         input.value = "";
     }
 });
 
 
 
-//Fetch data
+//Fetch data from the Restcountries.
 async function fetchData(input){
     try{
         const response = await axios.get(`https://restcountries.com/v3.1/name/${ input }`);
@@ -37,6 +39,7 @@ async function fetchData(input){
 
             //Checking if the lengt of languages array is larger than 1.
             let lang = Object.values(country.languages);
+            //text variable is changed depending on how many languages there are.
             let text = ""
             if (lang.length < 2){
                 lang = Object.values(country.languages)[0];
@@ -52,13 +55,13 @@ async function fetchData(input){
             }
 
 
-
+            //No flag because it does not fit the style of the website.
             const curr = Object.values(country.currencies)[0].name;
             countyInformation.innerHTML =`
                   <div class="outputDiv">
                         <p>Traveling is a journey, <span style="font-weight: bold; color: orange">${country.name.common}</span> to explore,
                             A chance to see the world, like never before.
-                            Visit the capital <span style="font-weight: bold; color: orange">${country.capital}</span>.
+                            Visit the capital <span style="font-weight: bold; color: orange">${country.capital}</span>. 
                             And venture beyond, to step out of the known,
                             To leave behind what's familiar, and go on our own.
                             The people we meet, <span style="font-weight: bold; color: orange">${country.population}</span> stories to share,
@@ -75,23 +78,14 @@ async function fetchData(input){
         }));
     }
     catch (e) {
+        //Error message into the index.html.
         countyInformation.innerHTML =`
                   <div class="outputDiv">
-                        <p>Oh no, it seems there's been a mistake,
+                        <p> Oh no, it seems there's been a <span style="font-weight: bold; color: orange">mistake</span>,
                             Perhaps a typo, or a slip-up you did make,
                             We cannot find the country you seek,
                             Please try again, and take another peek.
-                            
-                            Double check the spelling, it's important you see,
-                            Or you might be lost, like a ship at sea,
-                            Take your time, and try once more,
-                            And we'll help you find what you're looking for.
-                            
-                            Don't fret, don't fear, we're here to assist,
-                            With patience and care, until the issue is fixed,
-                            So take a deep breath, and give it a go,
-                            And together we'll find the answer, don't you know?
-                        </p>
+                            <span style="font-weight: bold; color: orange">Error code: 404 - Country not found</span>.
                    </div>
                 `;
         console.error(e);
